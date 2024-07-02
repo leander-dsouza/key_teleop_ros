66360c4b1244da62a05a3571f5d1d709c38dce99
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2024-07-02 09:02:10 +0100

    Node completed for ROS1

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # key_teleop_ros
-A ROS1/ROS2 package to drive a robot around using arrow keys.
+![ROS](https://img.shields.io/badge/-ROS-22314E?style=plastic&logo=ROS)
+![Python](https://img.shields.io/badge/-Python-black?style=plastic&logo=Python)
+
+A ROS1 package to drive a robot around using arrow keys.
+
+The following video is recorded with the [atreus](https://github.com/leander-dsouza/atreus) robot, embedding the key presses using [screenkey](https://www.thregr.org/wavexx/software/screenkey/).
+
+https://github.com/leander-dsouza/key_teleop_ros/assets/45683974/e2bec100-d9aa-4712-b843-7f26a5b68428
+
+## Installation
+
+* Install python dependencies using pip3:
+
+	```bash
+	pip3 install -r requirements.txt
+	```
+
+* Finally, install all the dependencies using `rosdep`:
+
+	```bash
+	rosdep install --from-paths $ROS_WS/src --ignore-src -r -y
+	```
+
+* Build the workspace:
+
+	```bash
+	catkin build key_teleop_ros
+	```
+
+## Usage
+
+* After launching your robot, you can execute the following command to drive it around using the arrow keys:
+
+    ```bash
+    rosrun key_teleop_ros key_drive.py
+    ```
+    Or you can execute the node using a launch file:
+
+    ```bash
+    roslaunch key_teleop_ros key_drive.launch
+    ```
+
+* The teleoperation node's parameters are as follows:
+    * `~update_rate` - The rate at which the node updates the robot's velocity. (default: 50 Hz)
+    * `~linear_vel_start` - The initial linear velocity of the robot. (default: 1.5 m/s)
+    * `~angular_vel_start` - The initial angular velocity of the robot. (default: 2.5 rad/s)
+    * `~max_linear_vel` - The maximum linear velocity of the robot. (default: 5.0 m/s)
+    * `~max_angular_vel` - The maximum angular velocity of the robot. (default: 5.0 rad/s)
+
+    The topic name can be remapped using the `cmd_vel` argument.
+
+* The following keys are used to drive and configure the robot:
+    * `↑` - Move forward
+    * `↓` - Move backward
+    * `←` - Turn left
+    * `→` - Turn right
+    * `w` - Increase linear velocity
+    * `s` - Decrease linear velocity
+    * `d` - Increase angular velocity
+    * `a` - Decrease angular velocity
+    * `q` - Quit the program
+
+## Standardization
+
+* The entire package is formatted against [pycodestyle](https://pypi.org/project/pycodestyle/) and [pydocstyle](https://pypi.org/project/pydocstyle/).
+
+###### 💾 EOF
--- scripts/key_drive.py
+++ scripts/key_drive.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 """Teleoperation using arrow keys for ROS1."""
 
-import argparse
 import rospy
 from geometry_msgs.msg import Twist, Vector3
 from pynput import keyboard
@@ -11,38 +10,37 @@ from pynput.keyboard import Key
 class KeyDrive():
     """Class to teleoperate the robot using arrow keys."""
 
-    def __init__(self, args):
+    def __init__(self):
         """Initialize the KeyDrive class."""
-        self.update_rate = args.rate
+        self.update_rate = rospy.get_param('~update_rate', 50)
         self.time_period = 1./self.update_rate
 
-        self.max_linear_velocity = args.max_linear_velocity
-        self.max_angular_velocity = args.max_angular_velocity
-        self.linear_velocity = args.linear_velocity
-        self.angular_velocity = args.angular_velocity
-        self.obj = Twist()
+        self.max_linear_vel = rospy.get_param('~max_linear_vel', 5.0)
+        self.max_angular_vel = rospy.get_param('~max_angular_vel', 5.0)
+        self.linear_vel = rospy.get_param('~linear_vel_start', 1.5)
+        self.angular_vel = rospy.get_param('~angular_vel_start', 2.5)
 
         # Publishers
-        self.pub = rospy.Publisher(args.topic_name, Twist, queue_size=10)
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # Timers
         rospy.Timer(rospy.Duration(self.time_period), self.keyboard_update)
 
     def forward(self):
         """Move Forward."""
-        self.pub.publish(Twist(linear=Vector3(x=self.linear_velocity)))
+        self.pub.publish(Twist(linear=Vector3(x=self.linear_vel)))
 
     def backward(self):
         """Move Backward."""
-        self.pub.publish(Twist(linear=Vector3(x=-self.linear_velocity)))
+        self.pub.publish(Twist(linear=Vector3(x=-self.linear_vel)))
 
     def left(self):
         """Move Left."""
-        self.pub.publish(Twist(angular=Vector3(z=self.angular_velocity)))
+        self.pub.publish(Twist(angular=Vector3(z=self.angular_vel)))
 
     def right(self):
         """Move Right."""
-        self.pub.publish(Twist(angular=Vector3(z=-self.angular_velocity)))
+        self.pub.publish(Twist(angular=Vector3(z=-self.angular_vel)))
 
     def brutestop(self):
         """Stop the robot."""
@@ -60,31 +58,31 @@ class KeyDrive():
             elif key == Key.left:
                 self.left()
             elif key.char == 'w':
-                if self.linear_velocity < self.max_linear_velocity:
-                    self.linear_velocity += 0.1
-                    self.linear_velocity = round(self.linear_velocity, 1)
-                    rospy.loginfo(f"Linear Velocity: {self.linear_velocity}")
+                if self.linear_vel < self.max_linear_vel:
+                    self.linear_vel += 0.1
+                    self.linear_vel = round(self.linear_vel, 1)
+                    rospy.loginfo(f"Linear Velocity: {self.linear_vel}")
                 else:
                     rospy.loginfo("Reached Max Linear Velocity")
             elif key.char == 's':
-                if self.linear_velocity > 0.1:
-                    self.linear_velocity -= 0.1
-                    self.linear_velocity = round(self.linear_velocity, 1)
-                    rospy.loginfo(f"Linear Velocity: {self.linear_velocity}")
+                if self.linear_vel > 0.1:
+                    self.linear_vel -= 0.1
+                    self.linear_vel = round(self.linear_vel, 1)
+                    rospy.loginfo(f"Linear Velocity: {self.linear_vel}")
                 else:
                     rospy.loginfo("Reached Minimal Linear Velocity")
             elif key.char == 'd':
-                if self.angular_velocity < self.max_angular_velocity:
-                    self.angular_velocity += 0.1
-                    self.angular_velocity = round(self.angular_velocity, 1)
-                    rospy.loginfo(f"Angular Velocity: {self.angular_velocity}")
+                if self.angular_vel < self.max_angular_vel:
+                    self.angular_vel += 0.1
+                    self.angular_vel = round(self.angular_vel, 1)
+                    rospy.loginfo(f"Angular Velocity: {self.angular_vel}")
                 else:
                     rospy.loginfo("Reached Max Angular Velocity")
             elif key.char == 'a':
-                if self.angular_velocity > 0.1:
-                    self.angular_velocity -= 0.1
-                    self.angular_velocity = round(self.angular_velocity, 1)
-                    rospy.loginfo(f"Angular Velocity: {self.angular_velocity}")
+                if self.angular_vel > 0.1:
+                    self.angular_vel -= 0.1
+                    self.angular_vel = round(self.angular_vel, 1)
+                    rospy.loginfo(f"Angular Velocity: {self.angular_vel}")
                 else:
                     rospy.loginfo("Reached Minimal Angular Velocity")
             elif key.char == 'q':
@@ -101,7 +99,7 @@ class KeyDrive():
 
     def keyboard_update(self, _):
         """Keyboard Listener for a press and release event."""
-        with keyboard.Listener(on_press=self.key_press, suppress=True) \
+        with keyboard.Listener(on_press=self.key_press) \
                 as listener_for_key_press:
             listener_for_key_press.join()
 
@@ -112,24 +110,6 @@ class KeyDrive():
 
 def main():
     """Mimic Main Function to initialize the ROS Node and KeyDrive class."""
-    parser = argparse.ArgumentParser(
-        description='ROS1 Teleoperation using Arrow Keys')
-    parser.add_argument('-r', '--rate', type=int, default=50,
-                        help='Update Rate for the Teleoperation')
-    parser.add_argument('-t', '--topic_name', type=str, default='/cmd_vel',
-                        help='Topic to publish Twist messages')
-    parser.add_argument('-l', '--linear_velocity', type=float, default=1.5,
-                        help='Maximum Linear Velocity')
-    parser.add_argument('-a', '--angular_velocity', type=float, default=2.5,
-                        help='Maximum Angular Velocity')
-    parser.add_argument('-ml', '--max_linear_velocity', type=float,
-                        default=5.0,
-                        help='Maximum Linear Velocity')
-    parser.add_argument('-ma', '--max_angular_velocity', type=float,
-                        default=5.0,
-                        help='Maximum Angular Velocity')
-    args = parser.parse_args()
-
     # Initialize ROS Node
     rospy.init_node('key_teleop_node')
 
@@ -140,7 +120,7 @@ def main():
         q: Quit\n")
 
     # Initialize KeyDrive
-    KeyDrive(args)
+    KeyDrive()
     rospy.spin()
     rospy.signal_shutdown("Done")
 
