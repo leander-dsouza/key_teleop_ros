7b25e9bf7f7188468fde0839d31330c3b796cd6e
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-30 19:19:04 +0100

    Added missing resource file and updated setup.py. (#3)
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.py')),
     ],
